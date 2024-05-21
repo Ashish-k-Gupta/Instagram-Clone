96c53323d657a17bb3c6baa7d3ece0ffcd918ca7
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-05-21 17:30:56 +0530

    working on comment section part, some problem with alignment of component needs to be fixed, don't know how to fix it

---
+++ src/Components/Comment/Comment.jsx
@@ -0,0 +1,8 @@
+
+const Comment = ({createdAt, username, profilePic, text}) => {
+  return (
+    <div>Comment</div>
+  )
+}
+
+export default Comment
--- src/Components/Profile/ProfilePost.jsx
+++ src/Components/Profile/ProfilePost.jsx
@@ -1,7 +1,8 @@
-import { Flex, GridItem, Text, Image,useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody } from '@chakra-ui/react'; 
+import { Flex, GridItem, Text, Image,useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Avatar, Box, Divider, VStack } from '@chakra-ui/react'; 
 import {AiFillHeart} from 'react-icons/ai';
 import {FaComment} from 'react-icons/fa';
-
+import { MdDelete } from "react-icons/md";
+import Comment from '../Comment/Comment';
 
 const ProfilePost = ({img}) => {
   const {isOpen, onOpen, onClose} = useDisclosure()
@@ -53,12 +54,63 @@ const ProfilePost = ({img}) => {
 
     </GridItem>
 
-<Modal isOpen={isOpen} onClose={onClose}>
+<Modal isOpen={isOpen} onClose={onClose}
+isCentered={true}
+size={{base:"3x1", md: "5x1  "}}
+>
   <ModalOverlay/>
   <ModalContent>
-    <ModalHeader>X</ModalHeader>
+    <ModalHeader/>
     <ModalCloseButton/>
-    <ModalBody>Hey this is a modal</ModalBody>
+    <ModalBody bg={'black'} pb={5}>
+      <Flex gap={4} w={{base: "90%", sm: "70%", md: "full"}} mx={'auto'}>
+        <Box
+         borderRadius={4}
+         overflow={"hidden"}
+         border={"1px solid"}
+         borderColor={"whiteAlpha.300"}
+         Flex={1.5}
+        >
+          <Image src={img} alt='profile post' />
+        </Box>
+        <Flex flex={1} flexDir={"column"} px={10} display={{base: "none", md: "flex"}}>
+          <Flex alignItems={"center"} justifyContent={"space-between"}>
+
+          <Flex alignItems={'center'} gap={4}>
+            <Avatar src='/profilepic.png' size={'sm'} name='Ashish Gupta'/>
+            <Text fontWeight={'bold'} fontSize={12}>Ashish Gupta</Text>
+          </Flex>
+          <Box _hover={{bg: "whiteAlpha.300", color: "red.600"}} borderRadius={4} p={1}>
+            <MdDelete size={20} cursor='pointer'/>
+          </Box>
+        </Flex>
+        <Divider my={4} bg={'gray.500'}/>
+        <VStack w='full' alignItems={'start'} maxH={'350px'} overflow={'auto'}>
+          <Comment
+          createdAt="1d ago"
+          username="Jasmine"
+          profilePic="/profilepic.png"
+          text={"Dummy images from unsplash"}
+          />
+          <Comment
+          createdAt="12h ago"
+          username="Lovepreet"
+          profilePic="/profilepic.png"
+          text={"Nice pic"}
+          />
+          <Comment
+          createdAt="3h ago"
+          username="Nikita"
+          profilePic="/profilepic.png"
+          text={"Good work dude!"}
+          />
+        </VStack>
+        </Flex>
+      </Flex>
+      
+      Hey this is a modal
+      
+      </ModalBody>
   </ModalContent>
 </Modal>
 
