d183df4062666aa4c1524c38ac9b5eb2952a60cc
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-06-10 16:49:23 +0530

    "Working on functionality that if a userprofile is not stored in database it won't let the user see it, there is a bug in (usegetUserProfileByUsername) will resolve that tomorros not stored in database it won't let the user see it, there is a bug in (usegetUserProfileByUsername"

--- test.jsx
+++ test.jsx
@@ -1,60 +1,68 @@
-import { Input, Button, Alert, AlertIcon } from "@chakra-ui/react";
-import { useState } from "react";
-import useLogin from "../../hooks/useLogin.js";
+import { Container, Flex, Link, Skeleton, SkeletonCircle, Text, VStack } from "@chakra-ui/react";
+import ProfileHeader from "../../components/Profile/ProfileHeader";
+import ProfileTabs from "../../components/Profile/ProfileTabs";
+import ProfilePosts from "../../components/Profile/ProfilePosts";
+import useGetUserProfileByUsername from "../../hooks/useGetUserProfileByUsername";
+import { useParams } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 
-const Login = () => {
-  const [inputs, setInputs] = useState({
-    email: "",
-    password: ""
-  });
-  const { loading, error, login } = useLogin();
+const ProfilePage = () => {
+	const { username } = useParams();
+	const { isLoading, userProfile } = useGetUserProfileByUsername(username);
 
-  const handleSubmit = (e) => {
-    e.preventDefault(); // Prevent the default form submission
-    login(inputs);
-  };
+	const userNotFound = !isLoading && !userProfile;
+	if (userNotFound) return <UserNotFound />;
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <Input 
-        placeholder="Email" 
-        fontSize={14} 
-        type="email"
-        value={inputs.email}
-        size="sm"
-        onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-        mb={2} // Adding margin for better spacing
-      />
-
-      <Input 
-        placeholder="Password" 
-        fontSize={14} 
-        type="password"
-        value={inputs.password}
-        size="sm"
-        onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-        mb={2} // Adding margin for better spacing
-      />
-
-      {error && (
-        <Alert status="error" fontSize={13} p={2} borderRadius={4} mb={2}>
-          <AlertIcon fontSize={12} />
-          {error.message}
-        </Alert>
-      )}
-
-      <Button 
-        w="full" 
-        colorScheme="blue" 
-        size="sm" 
-        fontSize={14}
-        isLoading={loading}
-        type="submit" // Change to submit type
-      >
-        Log in
-      </Button>
-    </form>
-  );
+	return (
+		<Container maxW='container.lg' py={5}>
+			<Flex py={10} px={4} pl={{ base: 4, md: 10 }} w={"full"} mx={"auto"} flexDirection={"column"}>
+				{!isLoading && userProfile && <ProfileHeader />}
+				{isLoading && <ProfileHeaderSkeleton />}
+			</Flex>
+			<Flex
+				px={{ base: 2, sm: 4 }}
+				maxW={"full"}
+				mx={"auto"}
+				borderTop={"1px solid"}
+				borderColor={"whiteAlpha.300"}
+				direction={"column"}
+			>
+				<ProfileTabs />
+				<ProfilePosts />
+			</Flex>
+		</Container>
+	);
 };
 
-export default Login;
+export default ProfilePage;
+
+// skeleton for profile header
+const ProfileHeaderSkeleton = () => {
+	return (
+		<Flex
+			gap={{ base: 4, sm: 10 }}
+			py={10}
+			direction={{ base: "column", sm: "row" }}
+			justifyContent={"center"}
+			alignItems={"center"}
+		>
+			<SkeletonCircle size='24' />
+
+			<VStack alignItems={{ base: "center", sm: "flex-start" }} gap={2} mx={"auto"} flex={1}>
+				<Skeleton height='12px' width='150px' />
+				<Skeleton height='12px' width='100px' />
+			</VStack>
+		</Flex>
+	);
+};
+
+const UserNotFound = () => {
+	return (
+		<Flex flexDir='column' textAlign={"center"} mx={"auto"}>
+			<Text fontSize={"2xl"}>User Not Found</Text>
+			<Link as={RouterLink} to={"/"} color={"blue.500"} w={"max-content"} mx={"auto"}>
+				Go home
+			</Link>
+		</Flex>
+	);
+};
